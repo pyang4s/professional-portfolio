2c5df1c00899774632def2d5a6fe67ec9c74a56c
Pa Nhia Yang <97122982+panhiay@users.noreply.github.com>
2023-07-26 12:39:46 -0600

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Header />
+
+  );
+}
+
+export default App;
+
+export function Header(){
+  return (
+    <div className="App-header">
+      <div className="bottom">Pa Nhia Yang</div>
+    </div>
+  )
+}
