380b57e5eb0b11820ae40ef5400c25a0f636eacb
Pa Nhia Yang <97122982+panhiay@users.noreply.github.com>
2023-07-26 15:08:21 -0600

    Updated header, started on summary & portfolio sections.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import linkedInLogo from "./assets/logo/linkedInLogo.png";
+import gitHubLogo from "./assets/logo/gitHubLogo.png";
+import "./App.css";
+import { Projects, UserProfile } from "./utils/types";
 
-function App() {
+export default function App() {
+  const myProfile: UserProfile = {
+    name: "Pa Nhia Yang",
+    title: "Full Stack Developer",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    phone: "",
+    email: "",
+    summary: "This is my summary",
+  };
+
   return (
-    <Header />
-
+    <div className="App">
+      <Header {...myProfile} />
+      <Summary {...myProfile} />
+    </div>
   );
 }
 
-export default App;
-
-export function Header(){
+export function Header({ name, title }: UserProfile) {
+  const profileName = <div style={{ float: "left" }}> {name}</div>;
+  const profileTitle = (
+    <div style={{ color: "#ffb703", float: "right" }}>{title}</div>
+  );
   return (
-    <div className="App-header">
-      <div className="bottom">Pa Nhia Yang</div>
+    <>
+      <div className="App-header">
+        <Social />
+        <div className="bottom">
+          {profileName}|{profileTitle}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function Social() {
+  return (
+    <div className="top">
+      <a href="https://www.linkedin.com/in/yang-panhia/">
+        <img className="icon" src={linkedInLogo} />
+      </a>
+
+      <a href="https://github.com/pyang4s?tab=repositories">
+        <img className="icon" src={gitHubLogo} />
+      </a>
     </div>
-  )
+  );
 }
