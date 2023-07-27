ffc742a2e16df290dabf615b6975ce4436e889ce
Pa Nhia Yang <97122982+panhiay@users.noreply.github.com>
2023-07-27 09:08:57 -0600

    Created Summary and Portfolio sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import linkedInLogo from "./assets/logo/linkedInLogo.png";
+import gitHubLogo from "./assets/logo/gitHubLogo.png";
+import "./App.css";
+import { Projects, UserProfile } from "./utils/types";
 
-function App() {
+export default function App() {
+  const myProfile: UserProfile = {
+    name: "Pa Nhia Yang",
+    title: "Full-Stack Developer",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    phone: "",
+    email: "",
+    summary: "This is my summary",
+  };
+
   return (
-    <Header />
-
+    <div className="App">
+      <Header {...myProfile} />
+      <Summary {...myProfile} />
+      <Portfolio />
+    </div>
   );
 }
 
-export default App;
-
-export function Header(){
+export function Header({ name, title }: UserProfile) {
+  const profileName = <div style={{ float: "left" }}> {name}</div>;
+  const profileTitle = (
+    <div style={{ color: "#ffb703", float: "right" }}>{title}</div>
+  );
   return (
-    <div className="App-header">
-      <div className="bottom">Pa Nhia Yang</div>
+    <>
+      <div className="App-header">
+        <Social />
+        <div className="bottom">
+          {profileName}|{profileTitle}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function Social() {
+  return (
+    <div className="top">
+      <a href="https://www.linkedin.com/in/yang-panhia/">
+        <img className="icon" src={linkedInLogo} alt="LinkedIn" />
+      </a>
+
+      <a href="https://github.com/pyang4s?tab=repositories">
+        <img className="icon" src={gitHubLogo} alt="GitHub Repo" />
+      </a>
     </div>
-  )
+  );
+}
+
+export function Summary({ summary }: UserProfile) {
+  return (
+    <>
+      <div className="App-summary">{summary}</div>
+    </>
+  );
+}
+
+export function Portfolio() {
+  const project1: Projects = {
+    name: "Portfolio",
+    description: "This project is this portfolio itself",
+    imageUrl: "",
+    gitHubLink: "",
+    language: "Typescript",
+    framework: "React",
+  };
+
+  return (
+    <div className="inline">
+      <Project {...project1} />
+      <Project {...project1} />
+    </div>
+  );
+}
+
+export function Project({ name, description, imageUrl, gitHubLink }: Projects) {
+  return (
+    <div className="App-portfolio">
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <img className="project-image" src={imageUrl} alt="Project1" />
+    </div>
+  );
 }
---
+++ src/utils/types.tsx
@@ -0,0 +1,20 @@
+export type UserProfile = {
+  name: string;
+  title: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  phone: string;
+  email: string;
+  summary: string;
+};
+
+export type Projects = {
+  name: string;
+  description: string;
+  imageUrl: string;
+  gitHubLink: string;
+  language: string;
+  framework: string;
+};
