ae0e39878bf1222105f5553d60a598fe4fa88025
pyang4s <125227563+pyang4s@users.noreply.github.com>
2023-07-28 16:26:12 -0600

    Updated Summary and Portfolio section, set them in different pages using routers. (#3)
    Co-authored-by: Pa Nhia Yang <97122982+panhiay@users.noreply.github.com>

---
+++ src/utils/Navigation.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { ImHome } from "react-icons/im";
+import "../App.css";
+import { useLocation, NavLink } from "react-router-dom";
+import { UserProfile } from "./types";
+import { myProfile, socialLinks } from "./constants";
+import linkedInLogo from "../assets/logo/linkedInLogo.png";
+import gitHubLogo from "../assets/logo/gitHubLogo.jpeg";
+import emailLogo from "../assets/logo/emailLogo.png";
+
+export const Navigation = () => (
+  <>
+    <Header />
+  </>
+);
+
+export function Header() {
+  const location = useLocation();
+
+  return (
+    <div className="App-header">
+      <Social />
+      {location.pathname == "/" ? (
+        <HeaderGreeting {...myProfile} />
+      ) : (
+        <ProjectGreeting />
+      )}
+    </div>
+  );
+}
+
+export function Social() {
+  return (
+    <div className="top">
+      <a href={socialLinks.linkedinUrl}>
+        <img className="icon" src={linkedInLogo} alt="LinkedIn" />
+      </a>
+
+      <a href={socialLinks.gitHubUrl}>
+        <img className="icon" src={gitHubLogo} alt="GitHubRepo" />
+      </a>
+
+      <a href={socialLinks.email}>
+        <img className="icon" src={emailLogo} alt="Email" />
+      </a>
+    </div>
+  );
+}
+
+export function HeaderGreeting({ name, title }: UserProfile) {
+  const profileName = <div style={{ float: "left" }}> {name}</div>;
+  const profileTitle = (
+    <div style={{ color: "#ffb703", float: "right" }}>{title}</div>
+  );
+  return (
+    <div className="header-bottom">
+      {profileName} | {profileTitle}
+    </div>
+  );
+}
+
+export function ProjectGreeting() {
+  return (
+    <>
+      <div className="header-bottom">My Projects</div>
+      <NavLink end to="/">
+        <ImHome className="home-icon" size="45px" />
+      </NavLink>
+    </>
+  );
+}
